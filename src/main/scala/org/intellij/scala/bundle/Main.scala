40a76c8c3be0a1bfaa6d03e3967f6fa9b6fab072
Pavel Fatin <pavel.fatin@jetbrains.com>
2018-09-14 16:08:52 +0200

    v2018-09-14

--- src/main/scala/org/intellij/scala/bundle/Main.scala
+++ src/main/scala/org/intellij/scala/bundle/Main.scala
@@ -12,7 +12,7 @@ import scala.Function._
   * @author Pavel Fatin
   */
 object Main {
-  private val Version = "2018-09-10"
+  private val Version = "2018-09-14"
 
   def main(args: Array[String]): Unit = {
     val target = file("./target")
