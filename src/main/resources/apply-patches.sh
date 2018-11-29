3b25e214ceeb46834be1049e9685957a472703d6
Pavel Fatin <pavel.fatin@jetbrains.com>
2018-11-29 14:06:04 +0100

    IDEA patches updated

--- src/main/resources/apply-patches.sh
+++ src/main/resources/apply-patches.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/JetBrains/intellij-community/idea/182.4323.46/platform/platform-impl/src/com/intellij/ui/AppUIUtil.java
-wget https://raw.githubusercontent.com/JetBrains/intellij-community/idea/182.4323.46/platform/platform-impl/src/com/intellij/idea/StartupUtil.java
+#diff -u AppUIUtil.java.original AppUIUtil.java > AppUIUtil.java.patch 
+#diff -u StartupUtil.java.original StartupUtil.java > StartupUtil.java.patch
+wget https://raw.githubusercontent.com/JetBrains/intellij-community/idea/183.4284.148/platform/platform-impl/src/com/intellij/ui/AppUIUtil.java
+wget https://raw.githubusercontent.com/JetBrains/intellij-community/idea/183.4284.148/platform/platform-impl/src/com/intellij/idea/StartupUtil.java
 patch AppUIUtil.java AppUIUtil.java.patch 
 patch StartupUtil.java StartupUtil.java.patch
 javac -g:vars -cp lib/* AppUIUtil.java
 javac -g:vars -cp lib/* StartupUtil.java
 rm AppUIUtil$*.class
-rm StartupUtil*.class
+rm StartupUtil$*.class
 rm AppUIUtil.java
 rm StartupUtil.java
