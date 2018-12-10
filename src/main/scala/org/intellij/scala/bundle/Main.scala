eba503431f3c382bf46df8aeb99172a810d6e8fe
Pavel Fatin <pavel.fatin@jetbrains.com>
2018-12-10 14:55:38 +0100

    Info.plist: "NoJavaDistribution" -> "jdk-bundled"

--- src/main/scala/org/intellij/scala/bundle/Main.scala
+++ src/main/scala/org/intellij/scala/bundle/Main.scala
@@ -185,6 +185,9 @@ object Main {
         s"See https://github.com/JetBrains/intellij-scala-bundle for more info."
 
     private val MacPatches: Descriptor = {
+      case Idea.Bundle =>
+        matches("Info\\.plist") & edit(_.replaceFirst("NoJavaDistribution", "jdk-bundled")) |
+        any
       case Idea.Resources =>
         matches("data/config/options/jdk\\.table\\.xml") &
           edit(_.replaceAll("\\$APPLICATION_HOME_DIR\\$\\/jre", "\\$APPLICATION_HOME_DIR\\$/jdk/Contents/Home")) | any
