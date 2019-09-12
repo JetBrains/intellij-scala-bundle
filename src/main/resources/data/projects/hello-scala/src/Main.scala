b1651986594a6c7232dbf093d51b9d8b23a23eaa
Pavel Fatin <pavel.fatin@jetbrains.com>
2019-09-12 16:58:36 +0200

    Run configuration: Scala REPL

--- src/main/resources/data/projects/hello-scala/src/Main.scala
+++ src/main/resources/data/projects/hello-scala/src/Main.scala
@@ -1,13 +1,8 @@
 object Main {
   /*
-   IntelliJ IDEA tutorials:
-   https://www.jetbrains.com/idea/documentation/
-
-   Scala Plugin tutorials:
-   https://www.jetbrains.com/help/idea/scala.html
-
-   Scala tutorials:
-   http://docs.scala-lang.org/
+   IntelliJ IDEA tutorials: https://www.jetbrains.com/idea/documentation
+   Scala Plugin tutorials:  https://www.jetbrains.com/help/idea/scala.html
+   Scala tutorials:         http://docs.scala-lang.org
   */
 
   def main(args: Array[String]): Unit = {
